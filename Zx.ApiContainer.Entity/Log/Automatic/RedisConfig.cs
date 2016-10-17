f16faf2c9e9c5f59037f4f31b79b2314c88070ec
zxh <agrainofsand2011@126.com>
2016-10-17 15:21:05 +0800

    build framework(暂时缺少nuget包)2

---
+++ Zx.ApiContainer.Entity/Log/Automatic/RedisConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zx.ApiContainer.Entity.Attributes;
+
+namespace Zx.ApiContainer.Entity.Log.Automatic
+{
+    [Serializable]
+    public class RedisConfig
+    {
+        [PrimaryKeyAttribute]
+        [IdentityAttribute]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Redis地址
+        /// </summary>
+        public string RedisHost { get; set; }
+
+        /// <summary>
+        /// Redis端口号
+        /// </summary>
+        public string RedisPort { get; set; }
+    }
+}
