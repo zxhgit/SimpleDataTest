9c8c463ba3b99fce1911b27415814ff4f790aece
zxh <agrainofsand2011@126.com>
2016-12-29 15:43:59 +0800

    first

---
+++ SimpleDataDemo/2_Basics/FindAllMethodSamples.cs
@@ -0,0 +1,60 @@
+namespace SimpleDataDemo
+{
+    internal class FindAllMethodSamples
+    {
+        internal void RunAll()
+        {
+            ExampleRunner.RunQuery(
+                "Run FindAll with no arguments. Throw BadExpressionException",
+                db => db.Albums.FindAll());
+
+            ExampleRunner.RunQuery(
+                "Run FindAll(db.Albums.GenreId) - just the column name. Throws BadExpressionException",
+                db => db.Albums.FindAll(db.Albums.GenreId));
+
+            ExampleRunner.RunQuery(
+                "Run FindAll(1) - no column names. Throws BadExpressionException",
+                db => db.Albums.FindAll(1));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId = null). Malformed Simple Expression. Throws BadExpressionException",
+                db => db.Albums.FindAll(db.Albums.GenreId = null));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == \"a\"). Malformed Simple Expression. Throws FormatException",
+                db => db.Albums.FindAll(db.Albums.GenreId == "a"));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId != null).",
+                db => db.Albums.FindAll(db.Albums.GenreId != null));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == 1)",
+                db => db.Albums.FindAll(db.Albums.GenreId == 1));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId > 3)",
+                db => db.Albums.FindAll(db.Albums.GenreId > 3));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == 1 && db.Albums.ArtistId == 120)",
+                db => db.Albums.FindAll(db.Albums.GenreId == 1 && db.Albums.ArtistId == 120));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == 1 & db.Albums.ArtistId == 120)",
+                db => db.Albums.FindAll(db.Albums.GenreId == 1 & db.Albums.ArtistId == 120));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == 2 || db.Albums.ArtistId == 160)",
+                db => db.Albums.FindAll(db.Albums.GenreId == 2 || db.Albums.ArtistId == 160));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == 2 | db.Albums.ArtistId == 160)",
+                db => db.Albums.FindAll(db.Albums.GenreId == 2 | db.Albums.ArtistId == 160));
+
+            ExampleRunner.RunQuery(
+                "Albums.FindAll(db.Albums.GenreId == 1, db.Albums.ArtistId == 120). Throws BadExpressionException as only one SimpleExpression allowed in FindAll",
+                db => db.Albums.FindAll(db.Albums.GenreId == 1, db.Albums.ArtistId == 120));
+        }
+    }
+}
---
+++ SimpleDataDemo/JoinRunner.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Simple.Data;
+using SimpleDataDemo.POCO;
+
+namespace SimpleDataDemo
+{
+    internal static class JoinRunner
+    {
+        public static void RunDemo(string explanation, Func<dynamic, dynamic> dbQuery, string pocoType, bool isEager)
+        {
+            try
+            {
+                ShowExplanation(explanation);
+
+                var listener = new ExampleTraceListener();
+                Trace.Listeners.Add(listener);
+
+                dynamic db = Database.Open();
+                dynamic results = dbQuery(db);
+                ListReturnedProperties(results, pocoType, isEager);
+
+                ShowSql(listener);
+
+                Trace.Listeners.Remove(listener);
+            }
+            catch (Exception ex)
+            {
+                ShowException(ex);
+            }
+
+            Console.WriteLine("Press return");
+            Console.ReadLine();
+        }
+
+        private static void ShowException(Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex.GetType().FullName);
+            Console.WriteLine(ex.Message);
+            Console.ResetColor();
+        }
+
+        private static void ShowExplanation(string explanation)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(explanation);
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+
+        private static void ShowSql(ExampleTraceListener listener)
+        {
+            Console.WriteLine("--------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SQL Sent to database was:");
+            Console.WriteLine(listener.Output);
+            Console.ResetColor();
+        }
+
+        private static void ListReturnedProperties(dynamic results, string pocoType, bool isEager)
+        {
+            if (results == null)
+            {
+                Console.WriteLine("Query returned null");
+                return;
+            }
+
+            Console.WriteLine("Query returned a {0}", results.GetType().FullName);
+
+            switch (pocoType)
+            {
+                case "singleArtist":
+                    Artist artist = results;
+                    ShowArtistAndAlbum(artist, isEager);
+                    break;
+                case "IEnumerableArtist":
+                    IEnumerable<Artist> artists = results;
+                    foreach (Artist artist2 in artists)
+                    {
+                        Console.WriteLine(ShowArtistAndAlbum(artist2, isEager));
+                    }
+                    break;
+                case "ListDynamic":
+                    List<dynamic> artistList = results;
+                    foreach (dynamic artist3 in artistList)
+                    {
+                        Console.WriteLine(ShowArtistAndAlbum(artist3, isEager));
+                    }
+                    break;
+                default:
+                    if (results is SimpleRecord)
+                    {
+                        Console.WriteLine(ShowArtistAndAlbum(results, isEager));
+                    }
+                    else
+                    {
+                        foreach (dynamic result in results)
+                        {
+                            Console.WriteLine(ShowArtistAndAlbum(result, isEager));
+                        }
+                    }
+                    break;
+            }
+        }
+
+
+        private static string ShowArtistAndAlbum(dynamic artist, bool isEager)
+        {
+            var sb = new StringBuilder();
+
+            Console.WriteLine("Artist {0} ({1})", artist.ArtistId, artist.Name);
+
+            if (!isEager || (artist.Albums != null))
+            {
+                foreach (dynamic album in artist.Albums)
+                {
+                    Console.WriteLine("\t{0}", album.Title);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static string ShowArtistAndAlbum(Artist artist, bool isEager)
+        {
+            var sb = new StringBuilder();
+
+            Console.WriteLine("Artist {0} ({1})", artist.ArtistId, artist.Name);
+
+            if (!isEager || (artist.Albums != null))
+            {
+                foreach (Album album in artist.Albums)
+                {
+                    Console.WriteLine("\t{0}", album.Title);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
